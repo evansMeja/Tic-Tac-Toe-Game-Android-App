a93fccfc3ad5c31b0029bda42bad4a3e6abf6694
Evans Kiprotich Kiplagat <evanslagat911@gmail.com>
2020-03-24 20:23:41 +0300

    Added added two color for players

--- app/src/main/java/com/example/tictactoegame/MainActivity.kt
+++ app/src/main/java/com/example/tictactoegame/MainActivity.kt
@@ -28,8 +28,7 @@ class MainActivity : AppCompatActivity() {
             R.id.bu8 -> cellID = 8
             R.id.bu9 -> cellID = 9
         }
-          Toast.makeText(this,"Cell Id: "+cellID,Toast.LENGTH_SHORT).show()
-           playGame(cellID,buSelected)
+        playGame(cellID,buSelected)
     }
     var player1 = ArrayList<Int>()
     var player2 = ArrayList<Int>()
@@ -48,5 +47,71 @@ class MainActivity : AppCompatActivity() {
             activeplayer=1
         }
         buSelected.isEnabled=false
+        checkWinner()
+    }
+
+    private fun checkWinner() {
+        var winner=-1
+        // Rows
+        // Row 1
+        if(player1.contains(1) && player1.contains(2) && player1.contains(3)){
+            winner=1
+        }
+        if(player2.contains(1) && player2.contains(2) && player2.contains(3)){
+            winner=2
+        }
+
+        // Row 2
+        if(player1.contains(4) && player1.contains(5) && player1.contains(6)){
+            winner=1
+        }
+        if(player2.contains(4) && player2.contains(5) && player2.contains(6)){
+            winner=2
+        }
+
+        // Row 3
+        if(player1.contains(7) && player1.contains(8) && player1.contains(9)){
+            winner=1
+        }
+        if(player2.contains(7) && player2.contains(8) && player2.contains(9)){
+            winner=2
+        }
+
+
+
+        // Columns
+        // Col 1
+        if(player1.contains(1) && player1.contains(4) && player1.contains(7)){
+            winner=1
+        }
+        if(player2.contains(1) && player2.contains(4) && player2.contains(7)){
+            winner=2
+        }
+
+
+        // Col 2
+        if(player1.contains(2) && player1.contains(5) && player1.contains(8)){
+            winner=1
+        }
+        if(player2.contains(2) && player2.contains(5) && player2.contains(8)){
+            winner=2
+        }
+
+
+        // Col 3
+        if(player1.contains(3) && player1.contains(6) && player1.contains(9)){
+            winner=1
+        }
+        if(player2.contains(3) && player2.contains(6) && player2.contains(9)){
+            winner=2
+        }
+
+        if(winner != 1){
+            if (winner == 1){
+                Toast.makeText(this, "Player 1 worn ",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Player 2 worn ",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
